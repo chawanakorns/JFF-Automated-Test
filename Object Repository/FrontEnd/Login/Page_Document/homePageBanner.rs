7cf377a69f192bd056a9439bcb0538c9b15fb1ba
JJKung <jj10102546@gmail.com>
2024-03-24 14:45:48 +0700

    commit automate test [Front-end and back end]

---
+++ Object Repository/FrontEnd/Login/Page_Document/homePageBanner.rs
@@ -0,0 +1,182 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>homePageBanner</name>
+   <tag></tag>
+   <elementGuidId>9345515e-9a2d-4c1a-bcf7-4c00c2761636</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='carouselExampleIndicators']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#carouselExampleIndicators</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>9aba1e52-352e-4fa4-a864-22c78c0da65b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>carouselExampleIndicators</value>
+      <webElementGuid>d99c4e70-ea57-428f-b298-0636b939f563</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>carousel slide py-4</value>
+      <webElementGuid>1f925c0c-1107-4974-aa04-dc975ef04e03</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-bs-ride</name>
+      <type>Main</type>
+      <value>carousel</value>
+      <webElementGuid>89b76b73-7c13-4fd2-ae91-3317a3bee4ba</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+      
+        
+        
+        
+      
+      
+        
+          
+        
+        
+          
+        
+        
+          
+        
+      
+      
+        
+        Previous
+      
+      
+        
+        Next
+      
+    </value>
+      <webElementGuid>3873acac-e283-45f5-b226-524d8ae699a4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;carouselExampleIndicators&quot;)</value>
+      <webElementGuid>f9271234-9779-4177-b648-b7e59187c4cc</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//div[@id='carouselExampleIndicators']</value>
+      <webElementGuid>a54cede0-c6ca-42e0-a848-3dbbc5dd95c4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Contact us'])[1]/following::div[2]</value>
+      <webElementGuid>52533c94-fda6-41f4-80ec-255a6c083848</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Poetry'])[1]/following::div[2]</value>
+      <webElementGuid>a0dcd857-e428-45c4-9064-58de7a2fc2c7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//body/div/div</value>
+      <webElementGuid>3f090a70-b9f5-480f-9d54-449a6af38fe7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[@id = 'carouselExampleIndicators' and (text() = '
+      
+        
+        
+        
+      
+      
+        
+          
+        
+        
+          
+        
+        
+          
+        
+      
+      
+        
+        Previous
+      
+      
+        
+        Next
+      
+    ' or . = '
+      
+        
+        
+        
+      
+      
+        
+          
+        
+        
+          
+        
+        
+          
+        
+      
+      
+        
+        Previous
+      
+      
+        
+        Next
+      
+    ')]</value>
+      <webElementGuid>668c4ad6-112f-4998-bc65-90f6feb8a4b6</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/FrontEnd/Register/Page_Document/bannerHomepage.rs
@@ -0,0 +1,182 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>bannerHomepage</name>
+   <tag></tag>
+   <elementGuidId>371d5475-debd-4613-8867-79872297fca8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='carouselExampleIndicators']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#carouselExampleIndicators</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>63d9bf59-13d6-4b61-bed0-d8e7ef1b4648</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>carouselExampleIndicators</value>
+      <webElementGuid>09b27573-bd99-410e-ba24-131408794ff1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>carousel slide py-4</value>
+      <webElementGuid>1be671bb-9f74-43c9-9466-a2d7307828f4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-bs-ride</name>
+      <type>Main</type>
+      <value>carousel</value>
+      <webElementGuid>bcee0652-341b-4376-bfb3-cf478d4c1279</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+      
+        
+        
+        
+      
+      
+        
+          
+        
+        
+          
+        
+        
+          
+        
+      
+      
+        
+        Previous
+      
+      
+        
+        Next
+      
+    </value>
+      <webElementGuid>32d17d01-ee19-4095-bf7b-29a6b344afc4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;carouselExampleIndicators&quot;)</value>
+      <webElementGuid>518dfc11-498d-4de3-9382-e96f1bc59825</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//div[@id='carouselExampleIndicators']</value>
+      <webElementGuid>cfadb607-916b-46a1-b4f4-1047be281ddc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Contact us'])[1]/following::div[2]</value>
+      <webElementGuid>25449595-48dc-47e3-a5e0-fbad5eba2dd9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Poetry'])[1]/following::div[2]</value>
+      <webElementGuid>48d19707-6c24-402b-8a5b-f02fd63dd7a4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//body/div/div</value>
+      <webElementGuid>b3aa40f5-327d-4128-98f0-f19786a59ebc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[@id = 'carouselExampleIndicators' and (text() = '
+      
+        
+        
+        
+      
+      
+        
+          
+        
+        
+          
+        
+        
+          
+        
+      
+      
+        
+        Previous
+      
+      
+        
+        Next
+      
+    ' or . = '
+      
+        
+        
+        
+      
+      
+        
+          
+        
+        
+          
+        
+        
+          
+        
+      
+      
+        
+        Previous
+      
+      
+        
+        Next
+      
+    ')]</value>
+      <webElementGuid>f9e41101-b624-4cdb-94b5-e3a25642eb69</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
